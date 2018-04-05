a1d2f8cee632c3f15d6d05b4c2447e80163d6c5b
Samuel Parotte <samuel.it.services@outlook.be>
2018-04-05 18:05:22 +0200

    add service worker

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ let restaurants,
   cuisines
 var map
 var markers = []
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,6 +1,17 @@
 let restaurant;
 var map;
-
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 /**
  * Initialize Google map, called from HTML.
  */
