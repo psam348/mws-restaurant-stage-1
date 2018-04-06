c80181cd5729a1f231936afabb93d3a59340c021
Samuel Parotte <samuel.it.services@outlook.be>
2018-04-06 18:27:05 +0200

    fix: Responsiveness &  Accessibility
    following feedback
    - remove static width
    - more descriptive alt text for img
    
    in html
    - put breadcrumb in nav
    - add role to map
    - proper headings for restaurant name, reviews and list
    -change in css to match new headings

--- js/main.js
+++ js/main.js
@@ -152,11 +152,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
+  image.alt = `Image of ${restaurant.name} Restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -68,7 +68,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  image.alt = restaurant.name;
+  image.alt = `Image of ${restaurant.name} Restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
@@ -107,7 +107,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
