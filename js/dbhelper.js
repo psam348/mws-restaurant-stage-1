dd03e5598641b71aefc8234f237b3e1458df7eb0
Samuel Parotte <samuel.it.services@outlook.be>
2018-07-21 12:47:58 +0200

    change : xhr to fetch

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,26 +9,43 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 1337 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+  static fetchRestaurants(callback,id) {
+    //let xhr = new XMLHttpRequest();
+    let urlToFetch;
+    
+    if (!id){
+      urlToFetch= this.DATABASE_URL;
+    }else{
+      urlToFetch= this.DATABASE_URL + "/" + id;
+    }
+    // console.log(urlToFetch);
+    fetch(urlToFetch).then(response => {
+      response.json().then(restaurants=>{
         callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+      })
+    })
+    .catch(error => {
+      callback(`Request failed. Returned status of ${error}`, null);
+    })
+
+    // xhr.open('GET', DBHelper.DATABASE_URL);
+    // xhr.onload = () => {
+    //   if (xhr.status === 200) { // Got a success response from server!
+    //     const json = JSON.parse(xhr.responseText);
+    //     const restaurants = json;
+    //     callback(null, restaurants);
+    //   } else { // Oops!. Got an error from server.
+    //     const error = (`Request failed. Returned status of ${xhr.status}`);
+    //     callback(error, null);
+    //   }
+    // };
+    // xhr.send();
   }
 
   /**
@@ -150,7 +167,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
